40af6b036eee560eaceae82295f1c96d8737b287
Mr. Coxall <mr.coxall@mths.ca>
2018-01-09 12:53:48 -0500

    deleted old files
    removed all the original files

--- config.py
+++
@@ -1,8 +0,0 @@
-# Created by: Mr. Coxall
-# Created on: Dec 2017
-# Created for: ICS3U
-# This is a config file that is used to save global variables
-#     used by all the files in the project
-
-size_of_screen_x = None
-size_of_screen_y = None
